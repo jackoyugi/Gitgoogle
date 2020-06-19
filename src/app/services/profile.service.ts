16b324be3f3a5c1f4bcd5c49afb9f3f8c64f34b4
jackoyugi <oyugijack@gmail.com>
2020-06-19 16:19:15 +0300

    added section and function of calling profile repos

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -19,4 +19,7 @@ export class ProfileService {
     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
     //.map(res => res.json());
   }
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+  }
 }

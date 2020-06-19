87a4597c31d9a78402dc4bf61f27de3e13d62910
jackoyugi <oyugijack@gmail.com>
2020-06-19 14:11:53 +0300

    added a call function that provides username, login and company of the github profile and can be consolog

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http, Headers} from '@angular/http';
-import 'rxjs/add/operator/map';
+import {HttpClient} from '@angular/common/http';
+//import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +11,12 @@ export class ProfileService {
   private clientid = 'ab4e69ad1ad03f4c3c00';
   private clientsecret = '2c92fdf025450bc0b1bf5800d27c4c9c67c0124d';
 
-  constructor(private http:Http) { }
+  constructor(private http:HttpClient) { 
+    console.log("service is now ready!")
+    this.username = 'jackoyugi';
+  }
+  getProfileInfo(){
+    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+    //.map(res => res.json());
+  }
 }
